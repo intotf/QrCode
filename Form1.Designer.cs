def6d09a30997db9c82b4f6fbe1912688e8d61d8
intotf <intotf@163.com>
2017-08-02 11:54:35 +0800

    C# 二维码生成

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace QrCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbLogo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbSize = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbTxt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.tbLogo);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.cbSize);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.tbTxt);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(609, 546);
+            this.splitContainer1.SplitterDistance = 107;
+            this.splitContainer1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(164, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 12);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "选择 Logo：";
+            // 
+            // tbLogo
+            // 
+            this.tbLogo.Location = new System.Drawing.Point(241, 76);
+            this.tbLogo.Name = "tbLogo";
+            this.tbLogo.ReadOnly = true;
+            this.tbLogo.Size = new System.Drawing.Size(274, 21);
+            this.tbLogo.TabIndex = 12;
+            this.tbLogo.Click += new System.EventHandler(this.textBox2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "二维码大小：";
+            // 
+            // cbSize
+            // 
+            this.cbSize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbSize.FormattingEnabled = true;
+            this.cbSize.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15"});
+            this.cbSize.Location = new System.Drawing.Point(104, 77);
+            this.cbSize.Name = "cbSize";
+            this.cbSize.Size = new System.Drawing.Size(50, 20);
+            this.cbSize.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "待生成字符串：";
+            // 
+            // tbTxt
+            // 
+            this.tbTxt.Location = new System.Drawing.Point(104, 11);
+            this.tbTxt.Multiline = true;
+            this.tbTxt.Name = "tbTxt";
+            this.tbTxt.Size = new System.Drawing.Size(492, 58);
+            this.tbTxt.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(521, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "生成";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(607, 433);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 546);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "二维码生成工具 QQ:42309073";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbSize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbTxt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox tbLogo;
+        private System.Windows.Forms.Label label3;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ThoughtWorks.QRCode.Codec;
+
+namespace QrCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.cbSize.SelectedIndex = 3;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var codeTxt = this.tbTxt.Text;
+            if (string.IsNullOrEmpty(codeTxt) || codeTxt.Length > 300)
+            {
+                MessageBox.Show("输入的字符长度在 1 至 300之间");
+                return;
+            }
+            var CodeScale = int.Parse(this.cbSize.SelectedItem.ToString());
+            //打印出 preResponse.QrCode 对应的条码
+            Bitmap bt;
+            string enCodeString = this.tbTxt.Text;
+            QRCodeEncoder qrCodeEncoder = new QRCodeEncoder();
+            qrCodeEncoder.QRCodeEncodeMode = QRCodeEncoder.ENCODE_MODE.BYTE;
+            qrCodeEncoder.QRCodeErrorCorrect = QRCodeEncoder.ERROR_CORRECTION.H;
+            qrCodeEncoder.QRCodeScale = CodeScale;
+            qrCodeEncoder.QRCodeVersion = 0;
+            bt = qrCodeEncoder.Encode(enCodeString, Encoding.UTF8);
+            string filename = System.DateTime.Now.ToString("yyyyMMddHHmmss") + "0000" + (new Random()).Next(1, 10000).ToString() + ".jpg";
+            var filePath = System.Environment.CurrentDirectory;
+            var imgPath = filePath + filename;
+            //bt.Save(imgPath);
+            var img = bt;
+            var logoPath = this.tbLogo.Text;
+            if (!string.IsNullOrEmpty(logoPath))
+            {
+                if (System.IO.File.Exists(logoPath))
+                {
+                    System.IO.MemoryStream MStream = new System.IO.MemoryStream();
+                    img = (Bitmap)CombinImage(bt, logoPath);
+                }
+                else
+                {
+                    MessageBox.Show(string.Format("Logo文件：{0} 不存在.", logoPath));
+                    return;
+                }
+            }
+            this.pictureBox1.Image = img;
+        }
+
+
+        /// <summary>
+        /// 调用此函数后使此两种图片合并，类似相册，有个
+        /// 背景图，中间贴自己的目标图片
+        /// </summary>
+        /// <param name="imgBack">粘贴的源图片</param>
+        /// <param name="destImg">粘贴的目标图片</param>
+        public static Image CombinImage(Image imgBack, string destImg)
+        {
+            Image img = Image.FromFile(destImg);    //照片图片
+            if (img.Height != 65 || img.Width != 65)
+            {
+                img = KiResizeImage(img, 65, 65, 0);
+            }
+            Graphics g = Graphics.FromImage(imgBack);
+            g.DrawImage(imgBack, 0, 0, imgBack.Width, imgBack.Height);   //g.DrawImage(imgBack, 0, 0, 相框宽, 相框高);
+            //g.FillRectangle(System.Drawing.Brushes.White, imgBack.Width / 2 - img.Width / 2 - 1, imgBack.Width / 2 - img.Width / 2 - 1,1,1);//相片四周刷一层黑色边框
+            //g.DrawImage(img, 照片与相框的左边距, 照片与相框的上边距, 照片宽, 照片高);
+            g.DrawImage(img, imgBack.Width / 2 - img.Width / 2, imgBack.Width / 2 - img.Width / 2, img.Width, img.Height);
+            GC.Collect();
+            return imgBack;
+        }
+        /// <summary>
+        /// Resize图片
+        /// </summary>
+        /// <param name="bmp">原始Bitmap</param>
+        /// <param name="newW">新的宽度</param>
+        /// <param name="newH">新的高度</param>
+        /// <param name="Mode">保留着，暂时未用</param>
+        /// <returns>处理以后的图片</returns>
+        public static Image KiResizeImage(Image bmp, int newW, int newH, int Mode)
+        {
+            try
+            {
+                Image b = new Bitmap(newW, newH);
+                Graphics g = Graphics.FromImage(b);
+                // 插值算法的质量
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.DrawImage(bmp, new Rectangle(0, 0, newW, newH), new Rectangle(0, 0, bmp.Width, bmp.Height), GraphicsUnit.Pixel);
+                g.Dispose();
+                return b;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.DefaultExt = "图片文件|*.jpg|*.png";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                this.tbLogo.Text = openFileDialog1.FileName;
+            }
+        }
+
+
+    }
+}
